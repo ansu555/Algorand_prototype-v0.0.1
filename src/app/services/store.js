7b225d61ed81f64f4a74c675cc6727d3ca0ca78e
ansu555 <anik200365@gmail.com>
2025-10-21 16:07:42 +0530

    feat: Remove deprecated services and components for codebase cleanup

--- src/app/services/store.js
+++ src/app/services/store.js
@@ -2,19 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { cryptoApi } from "./cryptoApi";
 import { exchangeApi } from "./exchangeApi";
-import { cryptoNewsApi } from "./cryptoNewsApi";
 
 const store = configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
     [exchangeApi.reducerPath]: exchangeApi.reducer,
-    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       cryptoApi.middleware,
-      exchangeApi.middleware,
-      cryptoNewsApi.middleware
+      exchangeApi.middleware
     ),
 });
 
